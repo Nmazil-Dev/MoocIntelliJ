27c8f351c47180cd4b73b20d565f1ea0085e200a
Nmazil-Dev <nmazil68@gmail.com>
2021-09-02 15:59:47 -0500

    Added Part 4 and 5 projects

---
+++ moocjava/src/Part4Arrays/print_neatly.java
@@ -0,0 +1,20 @@
+package Part4Arrays;
+
+public class print_neatly {
+    public static void printNeatly(int[] array){
+        int index = 0;
+        while (index < array.length){
+            if (index == array.length - 1){
+                System.out.print(array[index]);
+                index++;
+            } else {
+                System.out.print( array[index] + ", ");
+                index++;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] array = {5, 4, 3, 2, 1};
+        printNeatly(array);
+    }
+}
