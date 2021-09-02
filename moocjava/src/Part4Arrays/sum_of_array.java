bdc0d949d4248707fbba389d547d6e6b9f55365b
Lets-Learn-Coding <nmazil68@gmail.com>
2021-09-02 07:26:08 -0500

    Added new exercises

---
+++ moocjava/src/Part4Arrays/sum_of_array.java
@@ -0,0 +1,18 @@
+package Part4Arrays;
+
+public class sum_of_array {
+    public static int sumOfNumbersInArray(int[] array){
+        int sum = 0;
+        for (int number: array){
+            sum += number;
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        int numbers[] = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println("The sum of the array is: " + sumOfNumbersInArray(numbers));
+    }
+}
