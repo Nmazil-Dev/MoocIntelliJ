f917d1c8acdd785e9b0c76eadea7d7b3f84fcd47
Lets-Learn-Coding <nmazil68@gmail.com>
2021-09-08 21:57:44 -0500

    Changed url for progress.py

--- moocjava/progress.py
+++ moocjava/progress.py
@@ -1,4 +1,4 @@
 #Python script to open mooc @ current progress
 import webbrowser
-mooc = "https://java-programming.mooc.fi/part-4/1-introduction-to-object-oriented-programming"
+mooc = "https://java-programming.mooc.fi/part-4/3-files-and-reading-data"
 webbrowser.open(mooc)
